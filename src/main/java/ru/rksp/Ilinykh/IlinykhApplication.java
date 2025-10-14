a9395e656eb53b1f0d3601bf1814ad4159a452f4
Liza Ilinykh <kostitsinaliza@mail.ru>
2025-10-15 00:03:45 +0200

    Project generated

---
+++ src/main/java/ru/rksp/Ilinykh/IlinykhApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.Ilinykh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlinykhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlinykhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/Ilinykh/IlinykhApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.Ilinykh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlinykhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
